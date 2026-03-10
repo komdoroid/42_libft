d5545f5b100ade8acec1cd930b200ab527e325d9
komdoroid <kom.commoc@gmail.com>
2026-03-10 22:55:42 +0900

    create atoi

---
+++ src/ft_atoi.c
@@ -0,0 +1,26 @@
+include "libft.h"
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	m;
+	int	res;
+
+	i = 0;
+	m = 1;
+	while (ft_isspace(str[i]))
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			m *= -1;
+		i++;
+	}
+	res = 0;
+	while ('0' <= str[i] && str[i] <= '9')
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return (res);
+}
