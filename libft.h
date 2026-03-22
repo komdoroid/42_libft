6864372d1f13a8f596804942e839546f4983d448
komdoroid <kom.commoc@gmail.com>
2026-03-22 13:00:40 +0900

    update libft.h

--- libft.h
+++ libft.h
@@ -6,6 +6,8 @@
 
 // stdlib
 int	ft_atoi(char *str);
+char	*ft_itoa(int num);
+void	*ft_calloc(size_t nmemb, size_t size);
 
 // ctype
 int	ft_isalnum(int c);
@@ -33,5 +35,6 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
