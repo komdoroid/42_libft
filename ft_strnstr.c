843daf0e8225bbb7660c274c99394959b6b434a6
komdoroid <kom.commoc@gmail.com>
2026-03-23 22:02:06 +0900

    create ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,17 +1,44 @@
-// #include "libft.h"
+#include "libft.h"
 
-char *strnstr(char *big, char *little, int len)
+char *ft_strnstr(const char *big, const char *little, int len)
 {
 	int	i;
+	int	j;
 
 	i = 0;
 	if (!*little)
-		return (big);
-	if (ft_strlen(little) > len)
-		return (NULL);
-	while (i < len)
+		return ((char *)big);
+	while (i < len && big[i] != '\0')
 	{
-		if (big[i] != '\0' && little[i] != '\0')
-			while (big[i] == little[i])
-
+		j = 0;
+		if (big[i] == little[j])
+		{
+			while (big[i + j] == little[j] && big[i + j] != '\0' && i + j < len)
+				j++;
+			if (little[j] == '\0')
+				return ((char *)big + i);
+		}
+		i++;
 	}
+	return (NULL);
+}
+
+int	main(void)
+{
+	char	big[] = "Im spiderman";
+	char	little[] = "spi";
+	char	*res;
+
+	res = ft_strnstr(big, little, 8);
+	if (res == NULL)
+	{
+		write(1, "Not found\n", 10);
+		return (0);
+	}
+	while (*res != '\0')
+	{
+		write(1, res, 1);
+		res++;
+	}
+	write(1, "\n", 1);
+}
