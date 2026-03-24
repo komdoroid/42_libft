b51462599f216c6d4ea961cafc56e3acdbdc9dd4
komdoroid <kom.commoc@gmail.com>
2026-03-24 22:03:03 +0900

    update libft

--- libft.h
+++ libft.h
@@ -37,4 +37,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
 
+# libft func
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+
 #endif
