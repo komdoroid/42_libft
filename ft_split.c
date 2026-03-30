282f769ef1a81380296535b231ca1a032ca9c11c
komdoroid <kom.commoc@gmail.com>
2026-03-30 22:12:16 +0900

    create ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,16 +6,17 @@ static size_t	count_words(char const *s, char c)
 	size_t	i;
 
 	i = 0;
-	count = 0
+	count = 0;
 	while (s[i] != '\0')
 	{
+		while (s[i] == c)
+			i++;
 		if (s[i] != c)
 		{
-			while (s[i] != c)
-				i++;
 			count++;
+			while (s[i] != c && s[i] != '\0')
+				i++;
 		}
-		i++;
 	}
 	return (count);
 }
@@ -28,25 +29,57 @@ char	**ft_split(char const *s, char c)
 	size_t	words;
 	size_t	count;
 
+	if (s == NULL)
+		return (NULL);
 	words = count_words(s, c);
-	ptr = (char **)malloc(sizeof(char *) * words);
+	ptr = (char **)malloc(sizeof(char *) * (words + 1));
+	if (ptr == NULL)
+		return (NULL);
 	i = 0;
 	count = 0;
-	while (s[i] != '\0')
+	while (s[i] != '\0' && s[i] != '\0')
 	{
-		if (s[i] != c)
+		while (s[i] == c)
+			i++;
+		if (s[i] != '\0')
 		{
 			j = 0;
-			while (s[i + j] != c)
+			while (s[i + j] != c && s[i + j] != '\0')
 				j++;
 			ptr[count] = (char *)malloc(sizeof(char) * (j + 1));
 			if (ptr[count] == NULL)
 				return (NULL);
-			ft_memcpy(ptr[count], s[i], j);
+			ft_memcpy(ptr[count], &s[i], j);
+			ptr[count][j] = '\0';
 			i += j;
 			count++;
 		}
 		i++;
 	}
+	ptr[words] = NULL;
 	return (ptr);
 }
+
+int	main(void)
+{
+	char	s[] = "as;ldhflkask;jio ;a;lskjio";
+	char	c = 'a';
+	char	**res;
+	int	i;
+	int	j;
+
+	res = ft_split(s, c);
+	i = 0;
+	while (res[i] != NULL)
+	{
+		j = 0;
+		while (res[i][j] != '\0')
+		{
+			write(1, &res[i][j], 1);
+			j++;
+		}
+		write(1, "\n", 1);
+		i++;
+	}
+}
+
