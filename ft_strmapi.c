794c28fa30c7f96bc3eccc40813404cc2c988b52
komdoroid <kom.commoc@gmail.com>
2026-04-01 22:50:08 +0900

    compmlete strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,13 +1,23 @@
 #include "libft.h"
 
-char	*ft_strmpi(char cost *s, char (*f)(unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	char	*res;
 	unsigned int	i;
 	unsigned int	len;
 
+	if (s == NULL)
+		return (NULL);
 	len = ft_strlen(s);
 	res = (char *)malloc(sizeof(char) * (len + 1));
 	if (res == NULL)
 		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		res[i] = f(i, s[i]);
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
 }
