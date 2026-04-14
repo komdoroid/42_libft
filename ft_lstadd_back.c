6fdb7b236645ab4b095c2d5b583d1b1a7840b6d8
komdoroid <kom.commoc@gmail.com>
2026-04-14 22:58:24 +0900

    created ft_lstdelone

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -17,34 +17,34 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	tail->next = new;
 }
 
-#include <stdio.h>
-
-void	print_list(t_list *lst)
-{
-	if (!lst)
-		return ;
-	while (lst)
-	{
-		printf("%s -> ", (char *)lst->content);
-		lst = lst->next;
-	}
-	printf("NULL\n");
-}
-
-int	main(void)
-{
-	t_list	*head;
-
-	head = NULL;
-	t_list	*node1 = ft_lstnew("A");
-	t_list	*node2 = ft_lstnew("B");
-	t_list	*node3 = ft_lstnew("C");
-
-	ft_lstadd_back(&head, node1);
-	ft_lstadd_back(&head, node2);
-	ft_lstadd_back(&head, node3);
-
-	print_list(head);
-
-	return(0);
-}
+// #include <stdio.h>
+// 
+// void	print_list(t_list *lst)
+// {
+// 	if (!lst)
+// 		return ;
+// 	while (lst)
+// 	{
+// 		printf("%s -> ", (char *)lst->content);
+// 		lst = lst->next;
+// 	}
+// 	printf("NULL\n");
+// }
+// 
+// int	main(void)
+// {
+// 	t_list	*head;
+// 
+// 	head = NULL;
+// 	t_list	*node1 = ft_lstnew("A");
+// 	t_list	*node2 = ft_lstnew("B");
+// 	t_list	*node3 = ft_lstnew("C");
+// 
+// 	ft_lstadd_back(&head, node1);
+// 	ft_lstadd_back(&head, node2);
+// 	ft_lstadd_back(&head, node3);
+// 
+// 	print_list(head);
+// 
+// 	return(0);
+// }
