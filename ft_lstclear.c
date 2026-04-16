b6d42969d0b3ba2d2880600b4f8f61421cc0ffcf
komdoroid <kom.commoc@gmail.com>
2026-04-16 22:14:17 +0900

    create main function in ft_lstclear

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -30,13 +30,42 @@ void	print_list(t_list *lst)
 		lst = lst->next;
 	}
 	printf("NULL\n");
+}
+
+void	delete_content(void *content)
+{
+	free(content);
+}
 
 int	main(void)
 {
 	t_list	*head = NULL;
-	t_list	*node1 = ft_lstnew("X");
-	t_list	*node2 = ft_lstnew("Y");
-	t_list	*node3 = ft_lstnew("Z");
+	char	*str1;
+	char	*str2;
+	char	*str3;
 
+	str1 = (char *)malloc(sizeof(char) * 10);
+	str2 = (char *)malloc(sizeof(char) * 10);
+	str3 = (char *)malloc(sizeof(char) * 10);
+	ft_strlcpy(str1, "Hello, ", 10);
+	ft_strlcpy(str2, "42 ", 10);
+	ft_strlcpy(str3, "Tokyo.", 10);
 
+	t_list	*node1 = ft_lstnew(str1);
+	t_list	*node2 = ft_lstnew(str2);
+	t_list	*node3 = ft_lstnew(str3);
+
+	ft_lstadd_back(&head, node1);
+	ft_lstadd_back(&head, node2);
+	ft_lstadd_back(&head, node3);
+
+	print_list(head);
+	ft_lstclear(&head, delete_content);
+	print_list(head);
+	if (head == NULL)
+		printf("head is NULL (OK)\n");
+	else
+		printf("head is NOT NULL (NG)\n");
+
+	return (0);
 }
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,22 +1,10 @@
 #include "libft.h"
 
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i)
-}
-
 int	ft_strlcpy(char *dst, char *src, int size)
 {
 	int	i;
-	int	dst_len;
 	int	src_len;
 
-	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
 	i = 0;
 	if (size <= 0)
@@ -24,7 +12,7 @@ int	ft_strlcpy(char *dst, char *src, int size)
 	while (i < size && src[i] != '\0')
 	{
 		dst[i] = src[i];
-		i++
+		i++;
 	}
 	dst[i] = '\0';
 	return (src_len);
