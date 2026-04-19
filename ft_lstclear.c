3b3907795cd58bd2fb45d13ea58e2078144cd819
komdoroid <kom.commoc@gmail.com>
2026-04-19 14:18:42 +0900

    update libft

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -18,54 +18,54 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	*lst = NULL;
 }
 
-#include <stdio.h>
-
-void	print_list(t_list *lst)
-{
-	if (!lst)
-		return ;
-	while (lst)
-	{
-		printf("%s -> ", (char *)lst->content);
-		lst = lst->next;
-	}
-	printf("NULL\n");
-}
-
-void	delete_content(void *content)
-{
-	free(content);
-}
-
-int	main(void)
-{
-	t_list	*head = NULL;
-	char	*str1;
-	char	*str2;
-	char	*str3;
-
-	str1 = (char *)malloc(sizeof(char) * 10);
-	str2 = (char *)malloc(sizeof(char) * 10);
-	str3 = (char *)malloc(sizeof(char) * 10);
-	ft_strlcpy(str1, "Hello, ", 10);
-	ft_strlcpy(str2, "42 ", 10);
-	ft_strlcpy(str3, "Tokyo.", 10);
-
-	t_list	*node1 = ft_lstnew(str1);
-	t_list	*node2 = ft_lstnew(str2);
-	t_list	*node3 = ft_lstnew(str3);
-
-	ft_lstadd_back(&head, node1);
-	ft_lstadd_back(&head, node2);
-	ft_lstadd_back(&head, node3);
-
-	print_list(head);
-	ft_lstclear(&head, delete_content);
-	print_list(head);
-	if (head == NULL)
-		printf("head is NULL (OK)\n");
-	else
-		printf("head is NOT NULL (NG)\n");
-
-	return (0);
-}
+// #include <stdio.h>
+// 
+// void	print_list(t_list *lst)
+// {
+// 	if (!lst)
+// 		return ;
+// 	while (lst)
+// 	{
+// 		printf("%s -> ", (char *)lst->content);
+// 		lst = lst->next;
+// 	}
+// 	printf("NULL\n");
+// }
+// 
+// void	delete_content(void *content)
+// {
+// 	free(content);
+// }
+// 
+// int	main(void)
+// {
+// 	t_list	*head = NULL;
+// 	char	*str1;
+// 	char	*str2;
+// 	char	*str3;
+// 
+// 	str1 = (char *)malloc(sizeof(char) * 10);
+// 	str2 = (char *)malloc(sizeof(char) * 10);
+// 	str3 = (char *)malloc(sizeof(char) * 10);
+// 	ft_strlcpy(str1, "Hello, ", 10);
+// 	ft_strlcpy(str2, "42 ", 10);
+// 	ft_strlcpy(str3, "Tokyo.", 10);
+// 
+// 	t_list	*node1 = ft_lstnew(str1);
+// 	t_list	*node2 = ft_lstnew(str2);
+// 	t_list	*node3 = ft_lstnew(str3);
+// 
+// 	ft_lstadd_back(&head, node1);
+// 	ft_lstadd_back(&head, node2);
+// 	ft_lstadd_back(&head, node3);
+// 
+// 	print_list(head);
+// 	ft_lstclear(&head, delete_content);
+// 	print_list(head);
+// 	if (head == NULL)
+// 		printf("head is NULL (OK)\n");
+// 	else
+// 		printf("head is NOT NULL (NG)\n");
+// 
+// 	return (0);
+// }
