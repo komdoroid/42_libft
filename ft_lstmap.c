ce605b720c86cd51dbfdcc2f6e0ce656e2100982
komdoroid <kom.commoc@gmail.com>
2026-04-20 22:40:42 +0900

    create ft_lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -2,8 +2,31 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-}
+	t_list	*head;
+	t_list	*node;
+	void	*content;
 
-int	main(void)
-{
+	if (!lst || !f || !del)
+		return (NULL);
+	head = NULL;
+	while (lst)
+	{
+		node = (t_list *)malloc(sizeof(t_list));
+		if (node == NULL)
+		{
+			ft_lstclaer(&head, del);
+			return (NULL);
+		}
+		content = f(lst->content);
+		if (content == NULL)
+		{
+			ft_lstclear(&head, del);
+			return (NULL);
+		}
+		node->content = content;
+		node->next = NULL;
+		ft_lstadd_back(&head, node);
+		lst = lst->next;
+	}
+	return (head);
 }
