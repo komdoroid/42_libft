2fb8a4402aa0df27d4e095d0a24e67813265071c
kkomurat <kom.commoc@gmail.com>
2026-04-23 22:42:52 +0900

    update header

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,6 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkomurat <kkomurat@student.42tokyo.jp      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/23 21:43:17 by kkomurat          #+#    #+#             */
+/*   Updated: 2026/04/23 22:15:11 by kkomurat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
 int	ft_isdigit(int c)
 {
 	if ('0' <= c && c <= '9')
 		return (c);
 	return (0);
 }
+
+// #include <ctype.h>
+// #include <stdio.h>
+// 
+// int	main(void)
+// {
+// 	int	c;
+// 
+// 	c =ft_isdigit('0');
+// 	printf("ft_isdigi result: %d\n", c);
+// 	c = isdigit('0');
+// 	printf("isdigit result = %d\n", c);
+// }
--- ft_strlen.c
+++ ft_strlen.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkomurat <kkomurat@student.42tokyo.jp      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/23 22:25:41 by kkomurat          #+#    #+#             */
+/*   Updated: 2026/04/23 22:25:52 by kkomurat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 size_t	ft_strlen(const char *str)
@@ -13,11 +25,13 @@ size_t	ft_strlen(const char *str)
 	return (len);
 }
 
+// #include <string.h>
 // #include <stdio.h>
 // 
 // int	main(void)
 // {
 // 	char	*str = "Im Blue Giant";
 // 
-// 	printf("res: %d\n", ft_strlen(str));
+// 	printf("ft_strlen: %zu\n", ft_strlen(str));
+// 	printf("strlen: %zu\n", strlen(str));
 // }
