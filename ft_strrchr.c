619966c90ee95a9ed1811c72430004cee5d0ef95
kkomurat <kom.commoc@gmail.com>
2026-05-09 14:51:08 +0900

    make last change pointer

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: kkomurat <kkomurat@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/25 09:01:38 by kkomurat          #+#    #+#             */
-/*   Updated: 2026/05/07 21:54:09 by kkomurat         ###   ########.fr       */
+/*   Updated: 2026/05/09 14:46:38 by kkomurat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,29 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int	i;
-	int	last;
+	size_t	i;
+	char	*last;
 
 	i = 0;
-	last = -1;
+	last = NULL;
 	while (s[i] != '\0')
 	{
 		if (s[i] == (char)c)
-			last = i;
+			last = (char *)&s[i];
 		i++;
 	}
 	if (s[i] == (char)c)
-		last = i;
-	if (last == -1)
-		return (NULL);
-	return ((char *)(s + last));
+		last = (char *)&s[i];
+	return (last);
 }
 
 // #include <unistd.h>
-//
+// 
 // int	main(void)
 // {
 // 	char	*str = "42tokyo";
 // 	char	*res;
-//
+// 
 // 	res = ft_strrchr(str, 'o');
 // 	while (*res != '\0')
 // 	{
